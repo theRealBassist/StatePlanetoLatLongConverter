86f49cbaa85ae7e7cdc80a126197a5bb3f99649b
theRealBassist <rangercam98@gmail.com>
2024-03-15 16:15:34 -0400

    Added LatLong to State Plane and State Plane to State Plane conversions. Refactored as well

--- StatePlanetoLatLongConverter.py
+++ StatePlanetoLatLongConverter.py
@@ -4,12 +4,56 @@ from tkinter import filedialog
 from tablib import Dataset
 import os
 
-def getFile(): #responsible for displaying the UI and collecting desired selections
-    selections = [] #to simplify calls later, the selections are stored in a single array
-    
+def mainMenu():
     print("State Plane to Latitude/Longitude Converter")
     print("===============")
 
+    print("[L] - State Plane to Latitude/Longitude Conversion")
+    print("[S] - Latitude/Longitude to State Plane Conversion")
+    print("[B] - State Plane to State Plane Conversion")
+    print("[X] - Exit")
+
+    userInput = input()
+
+    file = getFile()
+    data = importData(file)
+    if userInput == "L" or userInput == "l":
+        EPSGZone = getEPSG(True)
+        convertedCoordinates = convertCoordinates(data, EPSGZone, 4326, False)
+        newData = writeData(data, convertedCoordinates, False)
+    elif userInput == "S" or userInput == "s":
+        EPSGZone = getEPSG(False)
+        convertedCoordinates = convertCoordinates(data, 4326, EPSGZone, True)
+        newData = writeData(data, convertedCoordinates, True)
+    elif userInput == "B" or userInput == "b":
+        EPSGFrom = getEPSG(True)
+        EPSGTo = getEPSG(False)
+        convertedCoordinates = convertedCoordinates(data, EPSGFrom, EPSGTo, False)
+        newData = writeData(data, convertedCoordinates, True)
+    elif userInput == "X" or userInput == "x":
+        quit()
+    else:
+        print("This is not a valid input. Please try again")
+        mainMenu()
+    
+    exportData(file, newData)
+
+
+def getEPSG(gettingFrom):
+    if gettingFrom:
+        print("Please input the EPSG Zone that the coordinates are in.")
+        print("West Georgia is zone `2240`")
+    else:
+        print("Please input the EPSG Zone that you are converting to.")
+
+    try:
+        EPSGZone = int(input())
+    except:
+        print ("Please enter only digits for the EPSG Zone.")
+        getEPSG(gettingFrom)
+    return EPSGZone
+
+def getFile(): #responsible for displaying the UI and collecting desired selections
     while True:
         try:
             tkinter.Tk().withdraw()
@@ -17,44 +61,51 @@ def getFile(): #responsible for displaying the UI and collecting desired selecti
         except:
             workingFile = input("Select the directory which contains your coordinates. \n Please ensure your EASTING and WESTING columns are named as such.")
         if os.path.exists(workingFile): #pathlib is incompatible with pyinstaller, so I'm using os.path
-            EPSGZone = input("Please enter the EPSG zone of the coordinates to be converted, or press `Enter` to default to `2240` West Georgia.\n")
             return workingFile
+        elif workingFile == "":
+            break
         else:
             print("This is not a valid directory! Please try again.")
-            break
+            getFile()
 
 def importData(workingFile):
     with open(workingFile, 'r') as file:
         importedData = Dataset().load(file)
         return importedData
 
-def selectData(importedData):
-    transformer = Transformer.from_crs(f'EPSG:{EPSGZone}', "EPSG:4326", always_xy=True)
 
-    eastings = importedData['EASTING']
-    northings = importedData['NORTHING']
+def convertCoordinates(importedData, EPSGFrom, EPSGTo, LatLong):
+    transformer = Transformer.from_crs(f'EPSG:{EPSGFrom}', f'EPSG:{EPSGTo}', always_xy=True)
 
-    latitudes = []
-    longitudes = []
-    for easting, northing in zip(eastings, northings):
-        coordinate = transformer.transform(easting, northing)
-        longitudes.append(coordinate[0])
-        latitudes.append(coordinate[1])
+    if not LatLong:
+        x = importedData['EASTING']
+        y = importedData['NORTHING']
+    else:
+        x = importedData['LATITUDE']
+        y = importedData['LONGITUDE']
 
-    coordinates = [latitudes, longitudes]
-    return coordinates
+    convertedX = []
+    convertedY = []
+    for currentX, currentY in zip(x, y):
+        coordinate = transformer.transform(currentX, currentY)
+        convertedX.append(coordinate[0])
+        convertedY.append(coordinate[1])
 
-def writeData(file, data, selectedData):
-    data.append_col(selectedData[0], header="LATITUDE")
-    data.append_col(selectedData[1], header = "LONGITUDE")
+    convertedCoordinates = [convertedX, convertedY]
+    return convertedCoordinates
 
+def writeData(data, convertedCoordinates, statePlane):
+    if statePlane:
+        data.append_col(convertedCoordinates[1], header="NORTHING")
+        data.append_col(convertedCoordinates[0], header="EASTING")
+    else:
+        data.append_col(convertedCoordinates[0], header="LATITUDE")
+        data.append_col(convertedCoordinates[1], header = "LONGITUDE")
+    return data
+
+def exportData(file, data):
     outputPath = os.path.join(os.path.dirname(os.path.realpath(file)), "output.csv")
     with open(outputPath, 'w', newline='') as outputFile:
         outputFile.write(data.csv)
 
-EPSGZone = 2240
-
-file = getFile()
-data = importData(file)
-selectedData = selectData(data)
-writeData(file, data, selectedData)
+mainMenu()
